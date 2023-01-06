8285a869ab2656fea4d63a1558f8bbdf86749e5f
KaustubhPandey23 <38600735+KaustubhPandey23@users.noreply.github.com>
2023-01-06 11:18:29 +0530

    Add files via upload

---
+++ Linear Equation/LETest.java
@@ -0,0 +1,6 @@
+public class LETest {
+	public static void main(String[] args) {
+		String YOUREQUATION = "5/(t+4)=4";
+		new LinearEquation(YOUREQUATION);
+	}
+}
---
+++ Linear Equation/LETest2.java
@@ -0,0 +1,6 @@
+public class LETest2 {
+	public static void main(String[] args) {
+		String YOUREQUATION = "5/(t+1)=3+(9-8)";
+		new LinearEquation(YOUREQUATION);
+	}
+}
